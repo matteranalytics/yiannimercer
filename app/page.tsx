9c9daf27e90fde61cb7f330b2d8737357a38a91b
yiannimercer <yianni@matteranalytics.io>
2025-08-26 20:56:28 -0500

    Implemented BentoGrids and added public directory for SVG items

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import { CiCircleInfo } from "react-icons/ci";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          { name: "Home", link: "/", icon: <FaHome /> },
+          { name: "About", link: "#about", icon: <CiCircleInfo /> },
+          // { name: "Projects", link: "#projects", icon: <ProjectsIcon /> },
+          // { name: "Contact", link: "#contact", icon: <ContactIcon /> },
+        ]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
---
+++ components/ui/BentoGrid.tsx
@@ -0,0 +1,71 @@
+import { cn } from "@/lib/utils";
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg
+
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-4xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none relative",
+        className,
+      )}
+      style={{
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(59,59,68,1) 26%, rgba(93,108,111,1) 100%)",
+      }}
+    >
+      <div className={`${id === 6 && 'flex justify-center h-full'}`}>
+
+      </div>
+
+      <div className="group-hover/bento:translate-x-2 transition duration-200">
+
+        <div className="fmt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+          {title}
+        </div>
+        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
+};
