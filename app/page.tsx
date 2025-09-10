1fda2e9533449eea43dfd8457a52a5cb0ca37cb7
Yianni <yianni@matteranalytics.io>
2025-09-09 23:21:01 -0500

    Added Footer.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -15,14 +16,15 @@ export default function Home() {
         <FloatingNav navItems={[
           { name: "Home", link: "/", icon: <FaHome /> },
           { name: "About", link: "#about", icon: <CiCircleInfo /> },
-          // { name: "Projects", link: "#projects", icon: <ProjectsIcon /> },
-          // { name: "Contact", link: "#contact", icon: <ContactIcon /> },
+          { name: "Projects", link: "#projects"  },
+          { name: "Contact", link: "#contact" },
         ]} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
+        <Footer />
       </div>
     </main>
   );
--- data/index.ts
+++ data/index.ts
@@ -186,13 +186,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/yiannimercer"
   },
   {
     id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/yiannimercer"
   },
 ];
